c443008bd13bc8b9b258746a5811c03bc7b4ddd7
lorne <1991wangliang@gmail.com>
2024-01-01 21:47:48 +0800

    fix example

--- example-infrastructure-jpa/src/main/java/com/codingapi/example/infrastructure/jpa/jpa/repository/NodeEntityRepository.java
+++ example-infrastructure-jpa/src/main/java/com/codingapi/example/infrastructure/jpa/jpa/repository/NodeEntityRepository.java
@@ -1,7 +1,7 @@
 package com.codingapi.example.infrastructure.jpa.jpa.repository;
 
 import com.codingapi.example.infrastructure.jpa.entity.NodeEntity;
-import com.codingapi.springboot.fast.query.FastRepository;
+import com.codingapi.springboot.fast.jpa.repository.FastRepository;
 
 public interface NodeEntityRepository extends FastRepository<NodeEntity, Integer> {
 
