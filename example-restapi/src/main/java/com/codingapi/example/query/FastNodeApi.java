2c381ddd051babaa40de2ef30ad323d6e7be6c8c
lorne <1991wangliang@gmail.com>
2024-01-01 20:58:38 +0800

    fix example

--- example-restapi/src/main/java/com/codingapi/example/query/FastNodeApi.java
+++ example-restapi/src/main/java/com/codingapi/example/query/FastNodeApi.java
@@ -9,7 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FastController
-public interface FastDemoApi {
+public interface FastNodeApi {
 
 
     @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
